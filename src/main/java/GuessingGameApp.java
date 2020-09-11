2f905fbea4f99732abcdd16d9fd81a222e6c51a0
Millicent Duke <millicent.j.duke@gmail.com>
2020-09-11 02:18:08 -0400

    Added handling for iteration 5 (> and < feedback for wrong guesses)

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -43,6 +43,11 @@ public class GuessingGameApp {
                             lineDivider);
                     System.exit(0);
                 default:
+                    if (guessedNumber > actualNumber){
+                        System.out.println("Your number is too high.");
+                    } else {
+                        System.out.println("Your number is too low.");
+                    }
                     if (triesRemaining == 1) {
                         System.out.println("Better luck next time!");
                     } else {
