8848f20b50e513870c4f3bb5d2e593796e7ef369
Millicent Duke <millicent.j.duke@gmail.com>
2020-09-11 01:10:53 -0400

    Initial structure and iterations 1, 2, and 4

--- MILLIEs_PERSONAL_README.md
+++
@@ -1,3 +0,0 @@
-Actually, ignore the dates in this project's readme. Teach says just do the first three iterations tonight.
-
-We're allowed to do more, but don't feel pressured to go further. If there's time, there's nothing wrong with doing all of them tonight... if there's time. Don't stress on doing more than just the first three.
--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,7 +1,50 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
 
-    }
+        Scanner inputScanner = new Scanner(System.in);
 
+        String lineDivider = "============================================================";
+        System.out.println("Round and round and round she goes!");
+        System.out.println("Where she stops, nobody knows!");
+        System.out.println(lineDivider);
+        System.out.println("Please pick a number between 1 and 10!");
+        System.out.println(lineDivider);
+
+        int turnsRemaining = 3;
+        int actualNumber = 7;
+        while (turnsRemaining > 0) {
+            int guessedNumber = inputScanner.nextInt();
+            inputScanner.nextLine();
+            if (guessedNumber == actualNumber) {
+                System.out.println();
+                System.out.println(lineDivider);
+                System.out.println("¸,ø¤º°`°º¤ø,¸ You win! ¸,ø¤º°`°º¤ø,¸");
+                System.out.println(lineDivider);
+                System.exit(0);
+            }
+            switch (guessedNumber) {
+                case 0:
+                    System.out.println();
+                    System.out.println(lineDivider);
+                    System.out.println("Remember:");
+                    System.out.println("Choose an integer value between 1 and 10, then press Enter!");
+                    System.out.println(lineDivider);
+                    break;
+                case -1:
+                    System.out.println();
+                    System.out.println(lineDivider);
+                    System.out.println("Well, that's my ride. See 'ya later!");
+                    System.out.println(lineDivider);
+                    System.exit(0);
+                default:
+                    System.out.println("Better luck next time!");
+                    break;
+            }
+
+        }
+
+    }
 }
